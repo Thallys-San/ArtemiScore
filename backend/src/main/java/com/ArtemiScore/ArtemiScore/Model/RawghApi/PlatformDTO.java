924c7a21783fc4a5ae39cd9c433403689d8685d6
Thallys_San <ricardo.viasolo142@gmail.com>
2025-06-23 16:47:08 -0300

    Add files via upload

---
+++ backend/src/main/java/com/ArtemiScore/ArtemiScore/Model/RawghApi/PlatformDTO.java
@@ -0,0 +1,10 @@
+package com.ArtemiScore.ArtemiScore.Model.RawghApi;
+
+import lombok.Data;
+
+@Data
+public class PlatformDTO {
+    private Long id;
+    private String name;
+    private String slug;
+}
---
+++ backend/src/main/java/com/ArtemiScore/ArtemiScore/Model/RawghApi/StoreDTO.java
@@ -0,0 +1,10 @@
+package com.ArtemiScore.ArtemiScore.Model.RawghApi;
+
+import lombok.Data;
+
+@Data
+public class StoreDTO {
+    private Long id;
+    private String name;
+    private String domain;
+}
