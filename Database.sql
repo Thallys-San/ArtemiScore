412c05f9004ed9b5dff6c98a1ef79ed005ca33bf
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-06-23 20:30:16 -0300

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -5,7 +5,9 @@ CREATE TABLE usuarios (
    id INT AUTO_INCREMENT PRIMARY KEY,
    nome VARCHAR(255) NOT NULL,
    email VARCHAR(255) UNIQUE NOT NULL,
-   senha VARCHAR(255) NOT NULL
+   senha VARCHAR(255) NOT NULL,
+   descricao VARCHAR(300),
+   foto_perfil VARCHAR(600)
 );
 
 
