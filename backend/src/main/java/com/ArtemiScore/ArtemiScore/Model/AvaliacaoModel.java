32e15bdf516ad60555fd45fe247a4b9ddac15cb7
Thallys_San <ricardo.viasolo142@gmail.com>
2025-06-24 14:52:02 -0300

    Add files via upload

---
+++ backend/src/main/java/com/ArtemiScore/ArtemiScore/Model/AvaliacaoModel.java
@@ -0,0 +1,37 @@
+package com.ArtemiScore.ArtemiScore.Model;
+
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "avaliacoes")
+@Entity
+@Getter
+
+@Setter
+
+@NoArgsConstructor
+
+public class AvaliacaoModel {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column (nullable = false )
+    private Long usuario_id;
+    @Column (nullable = false)
+    private Long jogo_id;
+    @Column (nullable = false, length = 2)
+    private int nota;
+    private String comentario;
+    @Column ()
+    private LocalDate data_avaliacao;
+}
---
+++ backend/src/main/java/com/ArtemiScore/ArtemiScore/Model/RawghApi/ScreenshotDTO.java
@@ -0,0 +1,9 @@
+package com.ArtemiScore.ArtemiScore.Model.RawghApi;
+
+import lombok.Data;
+
+@Data
+public class ScreenshotDTO {
+    private Long id;
+    private String image;
+}
