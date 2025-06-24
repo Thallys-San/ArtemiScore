2e6167d26bc19a873e9b5e0ad185405ef271ba4e
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-06-24 13:59:56 -0300

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -2,7 +2,7 @@ CREATE DATABASE ArtemiScore;
 use ArtemiScore;
 
 CREATE TABLE usuarios (
-   id INT AUTO_INCREMENT PRIMARY KEY,
+   id BIGINT AUTO_INCREMENT PRIMARY KEY,
    nome VARCHAR(255) NOT NULL,
    email VARCHAR(255) UNIQUE NOT NULL,
    senha VARCHAR(255) NOT NULL,
@@ -12,9 +12,9 @@ CREATE TABLE usuarios (
 
 
 CREATE TABLE avaliacoes (
-  id INTEGER PRIMARY KEY auto_increment,
-  usuario_id INTEGER NOT NULL,
-  jogo_id INTEGER NOT NULL, -- id do jogo na API RAWG
+  id BIGINT PRIMARY KEY auto_increment,
+  usuario_id BIGINT NOT NULL,
+  jogo_id BIGINT NOT NULL, -- id do jogo na API RAWG
   nota INTEGER NOT NULL CHECK(nota BETWEEN 1 AND 10),
   comentario TEXT,
   data_avaliacao DATETIME DEFAULT CURRENT_TIMESTAMP,
