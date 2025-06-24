7f7eb9b1cc9b1eb2f59dd07619c76d00ea9361da
Thallys_San <ricardo.viasolo142@gmail.com>
2025-06-24 14:52:02 -0300

    Add files via upload

---
+++ backend/src/main/java/com/ArtemiScore/ArtemiScore/Service/UsuariosService.java
@@ -0,0 +1,45 @@
+package com.ArtemiScore.ArtemiScore.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.ArtemiScore.ArtemiScore.Model.UsuariosModel;
+import com.ArtemiScore.ArtemiScore.repository.UsuariosRepository;
+
+
+@Service
+public class UsuariosService {
+    @Autowired
+    private UsuariosRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public List<UsuariosModel> listarTodos(){
+        return repository.findAll();
+    }
+
+    public Optional<UsuariosModel> listarId(Long id){
+        return repository.findById(id);
+    }
+
+    public UsuariosModel salvar(UsuariosModel usuariosModel){
+        String senha=usuariosModel.getSenha();
+
+        if (senha!= null && !senha.matches("^\\$2[abyx]\\$.{56}$")) {
+         // Hasheia a senha antes de salvar
+        String criptografia=passwordEncoder.encode(usuariosModel.getSenha());
+        usuariosModel.setSenha(criptografia);   
+        }
+        
+        return repository.save(usuariosModel);
+    }
+
+    public void deletar(Long id){
+        repository.deleteById(id);
+    }
+}
