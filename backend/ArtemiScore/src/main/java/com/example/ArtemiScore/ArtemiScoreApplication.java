d4e9709f1ad68ebb46794fc085ea65bf104e51c9
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-06-23 21:39:01 -0300

    Add files via upload

---
+++ backend/ArtemiScore/src/main/java/com/example/ArtemiScore/ArtemiScoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.ArtemiScore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtemiScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtemiScoreApplication.class, args);
+	}
+
+}
