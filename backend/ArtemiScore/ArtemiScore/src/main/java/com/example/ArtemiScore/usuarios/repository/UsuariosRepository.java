0ce90cf52541dcd4d054f37b663c00f2795173f5
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-06-23 21:53:56 -0300

    Delete backend/ArtemiScore directory

--- backend/ArtemiScore/ArtemiScore/src/main/java/com/example/ArtemiScore/usuarios/repository/UsuariosRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.ArtemiScore.usuarios.repository;
-
-public interface UsuariosRepository {
-
-}
--- backend/ArtemiScore/ArtemiScore/src/main/java/com/example/ArtemiScore/usuarios/service/UsuariosService.java
+++
@@ -1,5 +0,0 @@
-package com.example.ArtemiScore.usuarios.service;
-
-public class UsuariosService {
-
-}
