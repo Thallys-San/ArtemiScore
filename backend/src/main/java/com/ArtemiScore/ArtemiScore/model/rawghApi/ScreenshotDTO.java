59fe9e2873972a1b57695088087e49474d1e46ab
Thallys_San <ricardo.viasolo142@gmail.com>
2025-06-25 14:33:37 -0300

    Delete backend directory

--- backend/src/main/java/com/ArtemiScore/ArtemiScore/model/rawghApi/ScreenshotDTO.java
+++
@@ -1,9 +0,0 @@
-package com.ArtemiScore.ArtemiScore.model.rawghApi;
-
-import lombok.Data;
-
-@Data
-public class ScreenshotDTO {
-    private Long id;
-    private String image;
-}
--- backend/src/main/java/com/ArtemiScore/ArtemiScore/repository/UsuariosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ArtemiScore.ArtemiScore.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.ArtemiScore.ArtemiScore.model.UsuariosModel;
-
-@Repository
-public interface UsuariosRepository extends JpaRepository<UsuariosModel, Long>{}
