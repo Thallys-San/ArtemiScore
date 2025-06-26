fa05f84d1a09d0a2eec5aa6875d149480cf2059a
Thallys_San <ricardo.viasolo142@gmail.com>
2025-06-26 16:39:09 -0300

    Add files via upload

---
+++ backend/src/test/java/com/ArtemiScore/ArtemiScore/ArtemiScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ArtemiScore.ArtemiScore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtemiScoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
