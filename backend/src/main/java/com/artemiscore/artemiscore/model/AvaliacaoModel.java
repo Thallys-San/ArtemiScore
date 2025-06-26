eccbdefed10b0cfa06e1f270503d6d7ba2d34331
Thallys_San <ricardo.viasolo142@gmail.com>
2025-06-26 16:39:09 -0300

    Add files via upload

---
+++ backend/src/main/java/com/artemiscore/artemiscore/model/AvaliacaoModel.java
@@ -0,0 +1,57 @@
+package com.artemiscore.artemiscore.model;
+
+
+
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@Entity
+@Table(name = "avaliacoes")
+@Getter
+@Setter
+
+@NoArgsConstructor
+public class AvaliacaoModel {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (nullable = false )
+    private Long usuario_id;
+
+    @Column (nullable = false)
+    private Long jogo_id;
+
+    @DecimalMin(value = "1.0", inclusive = true)
+    @DecimalMax(value = "5.0", inclusive = true)
+    @Column (nullable = false)
+    private Double nota;
+
+    @Column
+    private int tempoDeJogo;
+
+    @Column(nullable = false, length = 255)
+    private String plataforma;
+
+    @Column
+    private String comentario;
+
+    @Column
+    private LocalDate data_avaliacao;
+}
