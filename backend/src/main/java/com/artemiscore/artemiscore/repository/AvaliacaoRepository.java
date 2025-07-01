1f8c725ba3afd517cb33c7f912407733d2413814
Thallys-San <ricardo.viasolo142@gmail.com>
2025-07-01 19:49:37 -0300

    !

--- backend/src/main/java/com/artemiscore/artemiscore/repository/AvaliacaoRepository.java
+++ backend/src/main/java/com/artemiscore/artemiscore/repository/AvaliacaoRepository.java
@@ -2,7 +2,11 @@ package com.artemiscore.artemiscore.repository;
 
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.artemiscore.artemiscore.model.AvaliacaoModel;
