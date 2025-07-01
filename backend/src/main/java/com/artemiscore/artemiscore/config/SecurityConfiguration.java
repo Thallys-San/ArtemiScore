77fce9e3a6a383d494c1401b213e63b9a5fe86ab
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-07-01 16:36:01 -0300

    Update SecurityConfiguration.java

--- backend/src/main/java/com/artemiscore/artemiscore/config/SecurityConfiguration.java
+++ backend/src/main/java/com/artemiscore/artemiscore/config/SecurityConfiguration.java
@@ -2,7 +2,8 @@ package com.artemiscore.artemiscore.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,17 +14,36 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfiguration {
 
+    // Define o encoder de senha (mesmo que você já usa no UsuariosService)
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    // Expõe o AuthenticationManager, necessário para autenticação no backend
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)throws Exception{
+        return config.getAuthenticationManager();
+    }
+
+    // Define as regras de segurança (rotas públicas, privadas, login, etc)
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-            .httpBasic(Customizer.withDefaults())
-            .csrf(csrf -> csrf.disable());
-        return http.build();
+            .csrf(csfr -> csfr.disable()) // desativa CSRF para facilitar testes com Postman, por exemplo
+            .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/login","/cadastro","/css/**","/js/**").permitAll() //rotas publicas
+            .anyRequest().authenticated()// o resto precisa estar logado
+            )
+            .formLogin(form -> form
+            .loginPage("login")// página personalizada de login
+            .defaultSuccessUrl("home",true)// redireciona após login bem-sucedido
+            .permitAll()
+            )
+            .logout(logout -> logout
+            .logoutSuccessUrl("/login?logout")//redireciona apos logout
+            .permitAll()
+            );
+            return http.build();
     }
 }
