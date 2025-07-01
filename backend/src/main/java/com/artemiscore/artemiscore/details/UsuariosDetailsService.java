55fd31dd7cbb6476a9250ab74a0c5b00c5b333df
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-07-01 16:42:39 -0300

    Update UsuariosDetailsService.java

--- backend/src/main/java/com/artemiscore/artemiscore/details/UsuariosDetailsService.java
+++ backend/src/main/java/com/artemiscore/artemiscore/details/UsuariosDetailsService.java
@@ -11,18 +11,23 @@ import org.springframework.stereotype.Service;
 import com.artemiscore.artemiscore.model.UsuariosModel;
 import com.artemiscore.artemiscore.repository.UsuariosRepository;
 
+// Essa classe é usada pelo Spring Security para carregar usuários do banco de dados
 @Service
 public class UsuariosDetailsService implements UserDetailsService{
 
     @Autowired
     private UsuariosRepository repository;
 
+    // Método chamado automaticamente pelo Spring Security quando alguém tenta fazer login
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        // Busca o usuário no banco usando o e-mail
         Optional<UsuariosModel> usuario=repository.findByEmail(email);
+        // Se não encontrar, lança exceção
         if(usuario.isEmpty()){  
             throw new UsernameNotFoundException("Usuário não encontrado com e-mail:"+email);
         }
+        // Se encontrar, retorna um objeto do tipo UserDetails (adaptado pela classe UsuarioDetails)
         return new UsuariosDetails(usuario.get());
     }
 
