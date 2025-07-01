517e981453cb3991ef1dc1490f7439282c4c2238
Ryan Gabriel <ryangabrielsilvag052@gmail.com>
2025-07-01 16:28:37 -0300

    Update AvaliacaoRepository.java

--- backend/src/main/java/com/artemiscore/artemiscore/repository/AvaliacaoRepository.java
+++ backend/src/main/java/com/artemiscore/artemiscore/repository/AvaliacaoRepository.java
@@ -8,4 +8,8 @@ import org.springframework.stereotype.Repository;
 import com.artemiscore.artemiscore.model.AvaliacaoModel;
 
 @Repository
-public interface AvaliacaoRepository extends JpaRepository<AvaliacaoModel, Long>{}
+public interface AvaliacaoRepository extends JpaRepository<AvaliacaoModel, Long>{
+    @Query("SELECT a FROM AvaliacaoModel a WHERE a.jogo_id = :jogoId")
+    List<AvaliacaoModel> findAvaliacoesByJogoId(@Param("jogoId") Long jogoId);
+  
+}
