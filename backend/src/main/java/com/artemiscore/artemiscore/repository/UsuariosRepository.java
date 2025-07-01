179a2457de5ad39fbca39aae9dc489d067772d14
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-07-01 16:32:08 -0300

    Update UsuariosRepository.java

--- backend/src/main/java/com/artemiscore/artemiscore/repository/UsuariosRepository.java
+++ backend/src/main/java/com/artemiscore/artemiscore/repository/UsuariosRepository.java
@@ -2,10 +2,15 @@ package com.artemiscore.artemiscore.repository;
 
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.artemiscore.artemiscore.model.UsuariosModel;
 
 @Repository
-public interface UsuariosRepository extends JpaRepository<UsuariosModel, Long>{}
+public interface UsuariosRepository extends JpaRepository<UsuariosModel, Long>{
+    // Método customizado para buscar um usuário pelo e-mail
+    Optional<UsuariosModel> findByEmail(String email);
+}
