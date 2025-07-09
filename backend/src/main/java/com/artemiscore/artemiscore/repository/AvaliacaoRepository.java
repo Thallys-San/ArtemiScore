6f4adfc57dabb5603040667e46840dbe019890f4
Thallys_San <ricardo.viasolo142@gmail.com>
2025-07-09 16:50:21 -0300

    Add files via upload

---
+++ backend/src/main/java/com/artemiscore/artemiscore/repository/AvaliacaoRepository.java
@@ -0,0 +1,22 @@
+package com.artemiscore.artemiscore.repository;
+
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.artemiscore.artemiscore.model.AvaliacaoModel;
+
+@Repository
+public interface AvaliacaoRepository extends JpaRepository<AvaliacaoModel, Long>{
+    @Query("SELECT a FROM AvaliacaoModel a WHERE a.jogo_id = :jogoId")
+    List<AvaliacaoModel> findAvaliacoesByJogoId(@Param("jogoId") Long jogoId);
+
+    @Query("SELECT a FROM AvaliacaoModel a WHERE a.usuario_id = :usuarioId")
+    List<AvaliacaoModel> findAvaliacoesByUsuarioId(@Param("usuarioId") Long usuarioId);
+  
+}
