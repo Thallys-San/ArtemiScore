4100e9a36079d09f507af3e0739e52b498510ebc
Thallys_San <ricardo.viasolo142@gmail.com>
2025-07-09 16:50:21 -0300

    Add files via upload

---
+++ backend/src/main/java/com/artemiscore/artemiscore/model/rawghApi/GameCardDTO.java
@@ -0,0 +1,15 @@
+package com.artemiscore.artemiscore.model.rawghApi;
+
+import lombok.Data;
+
+@Data
+public class GameCardDTO {
+    private Long id;
+    private String name;
+    private String slug;
+    private String released;
+    private String background_image;
+    private String description;
+    private String description_raw;
+    private Double mediaAvaliacao;
+}
