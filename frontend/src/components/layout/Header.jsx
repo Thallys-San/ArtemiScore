a18241ee7206a6d8aaf34d6c6bd2c05e9f593182
Thallys-San <ricardo.viasolo142@gmail.com>
2025-07-31 02:42:32 -0300

    !

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+
 
 const Header = () => (
   <header>
     <div className="container header-container">
       <div className="flex items-center">
-        <Link to="/" className="logo font-pacifico">AtermiScore</Link>
+        <a href="#" className="logo font-pacifico">AtermiScore</a>
         <nav className="main-nav">
-          <Link to="/HomePage" className="nav-link active">Início</Link>
-          <Link to="/jogos" className="nav-link">Jogos</Link>
-          <Link to="/ranking" className="nav-link">Ranking</Link>
-          <Link to="/lancamentos" className="nav-link">Lançamentos</Link>
+          <a href="HomePage.jsx" className="nav-link active">Início</a>
+          <a href="Jogos.jsx" className="nav-link">Jogos</a>
+          <a href="rank.jsx" className="nav-link">Ranking</a>
+          <a href="lancamentos.jsx" className="nav-link">Lançamentos</a>
         </nav>
       </div>
       <div className="flex items-center gap-4">
@@ -25,27 +26,12 @@ const Header = () => (
             <i className="ri-user-line user-icon"></i>
           </div>
           <div className="user-dropdown" id="userDropdownMenu">
-            <Link to="/perfil" className="dropdown-item">
-              <div className="dropdown-icon"><i className="ri-user-3-line"></i></div>
-              <span>Meu Perfil</span>
-            </Link>
-            <Link to="/minhasavaliacoes" className="dropdown-item">
-              <div className="dropdown-icon"><i className="ri-star-line"></i></div>
-              <span>Minhas Avaliações</span>
-            </Link>
-            <Link to="/jogosfavoritos" className="dropdown-item">
-              <div className="dropdown-icon"><i className="ri-heart-line"></i></div>
-              <span>Jogos Favoritos</span>
-            </Link>
-            <Link to="/configuracoes" className="dropdown-item">
-              <div className="dropdown-icon"><i className="ri-settings-3-line"></i></div>
-              <span>Configurações</span>
-            </Link>
+            <a href="perfil.html" className="dropdown-item"><div className="dropdown-icon"><i className="ri-user-3-line"></i></div><span>Meu Perfil</span></a>
+            <a href="MInhasavaliações.html" className="dropdown-item"><div className="dropdown-icon"><i className="ri-star-line"></i></div><span>Minhas Avaliações</span></a>
+            <a href="Jogosfavoritos.html" className="dropdown-item"><div className="dropdown-icon"><i className="ri-heart-line"></i></div><span>Jogos Favoritos</span></a>
+            <a href="configurações.html" className="dropdown-item"><div className="dropdown-icon"><i className="ri-settings-3-line"></i></div><span>Configurações</span></a>
             <div className="dropdown-divider"></div>
-            <a href="#" className="dropdown-item">
-              <div className="dropdown-icon"><i className="ri-logout-box-line"></i></div>
-              <span>Sair</span>
-            </a>
+            <a href="#" className="dropdown-item"><div className="dropdown-icon"><i className="ri-logout-box-line"></i></div><span>Sair</span></a>
           </div>
         </div>
       </div>
