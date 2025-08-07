37f3a209aa5fc49ded414cfade5dd28f4b3096ba
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-08-07 00:41:23 -0300

    Update UsuariosController.java

--- backend/src/main/java/com/artemiscore/artemiscore/controller/UsuariosController.java
+++ backend/src/main/java/com/artemiscore/artemiscore/controller/UsuariosController.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -86,4 +87,14 @@ public class UsuariosController {
         return ResponseEntity.ok(response);
     }
 
+    @GetMapping("/me")
+    public ResponseEntity<UsuariosModel> getUsuarioLogado (Authentication authentication){
+        if (authentication==null || !authentication.isAuthenticated() ) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        String email = authentication.getName();// normalmente o e-mail
+        return service.buscarPorEmail(email)
+        .map(ResponseEntity::ok)
+        .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+    }
 }
