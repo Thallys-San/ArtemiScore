58c4f8e72a04561bc7dd77f484a9a19be96c6f14
Thallys_San <ricardo.viasolo142@gmail.com>
2025-08-08 18:56:29 -0300

    Update index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
 import "./style.css";
-//import { AuthProvider } from "./components/AuthContext";
+import { AuthProvider } from "./components/AuthContext";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    {/*<AuthProvider>*/}
+    <AuthProvider>
     <App />
-    {/*</AuthProvider>*/}
+    </AuthProvider>
   </BrowserRouter>
 );
